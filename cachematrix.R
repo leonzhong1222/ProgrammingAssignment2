046ae04d27cc0c9eefaaa718ffcf804f15919ed6
leonzhong1222 <leonzhong1222@outlook.com>
2025-02-26 14:48:53 +0800

    LEON cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,13 @@
-## Put comments here that give an overall description of what your
-## functions do
+# Create a matrix
+mat <- matrix(c(4, 3, 3, 2), nrow = 2, ncol = 2)
 
-## Write a short comment describing this function
+# Create the special "matrix" object
+cacheMat <- makeCacheMatrix(mat)
 
-makeCacheMatrix <- function(x = matrix()) {
+# Compute the inverse (not cached yet)
+inverse1 <- cacheSolve(cacheMat)
+print(inverse1)
 
-}
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+# Retrieve the inverse from the cache
+inverse2 <- cacheSolve(cacheMat)  # This will print "getting cached data"
+print(inverse2)
